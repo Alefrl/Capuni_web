1040ea3f983f233058cc153def8f7a885405ff45
Alessio Capuni <alessiocapuni07@gmail.com>
2026-03-24 14:07:56 +0000

    calc

--- process.argv/calc.js
+++ process.argv/calc.js
@@ -1,16 +1,16 @@
 //somma
-if (process.argv[3] === "som") {
+if (process.argv[3] === "somma") {
   console.log(Number(process.argv[2]) + Number(process.argv[4]));
 }
 //sottrazione
-else if (process.argv[3] === "sot") {
+else if (process.argv[3] === "sottra") {
   console.log(Number(process.argv[2]) - Number(process.argv[4]));
 }
 //moltiplicazione
-else if (process.argv[3] === "mol") {
+else if (process.argv[3] === "molti") {
   console.log(Number(process.argv[2]) * Number(process.argv[4]));
 }
 //divisione
-else if (process.argv[3] === "div") {
+else if (process.argv[3] === "dividi") {
   console.log(Number(process.argv[2]) / Number(process.argv[4]));
 }
